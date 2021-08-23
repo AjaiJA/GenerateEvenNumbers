8cb8ce9afdb9d80a9f4e492230d5d84389501ab8
AjaiJA <ajaija2000@gmail.com>
2021-08-23 13:57:09 +0530

    Files Added

---
+++ Even.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class Even{
+    public static void main(String args[]){
+        int n=10;
+        for (int i = 2; i <= n; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
